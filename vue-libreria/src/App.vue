5dca3f1dad1180960b65ab074dd2e2db9c21e943
Enrique-Mora-Zavala <L21390359@chetumal.tecnm.mx>
2026-06-06 10:08:50 -0500

    Se aplican estilos a los links de listar libros y de crear nuevos libros

--- vue-libreria/src/App.vue
+++ vue-libreria/src/App.vue
@@ -5,9 +5,9 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Libros</RouterLink>
-        <RouterLink to="/nuevoLibro">Nuevo libro</RouterLink>
+      <nav class="navbar">
+        <RouterLink class="link--navbar"to="/">Libros</RouterLink>
+        <RouterLink class="link--navbar" to="/nuevoLibro">Nuevo libro</RouterLink>
       </nav>
     </div>
   </header>
@@ -16,5 +16,24 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
+  .navbar {
+    background-color: rgba(0, 0, 0, 0.1);
+    margin: 10px auto;
+    width: 300px;
+    display: flex;
+    justify-content: space-around;
+    border-radius: 5px;
+  }
 
+  .link--navbar {
+    text-decoration: none;
+    padding: 10px;
+    font-weight: 700;
+    font-size: 1.3rem;
+    color: #181818;
+  }
+
+  .link--navbar:hover {
+    color: #000;
+  }
 </style>
